31b5208fc83cb20583c7290898338f802f34133f
melonsoda20 <benedictusteddy200596@gmail.com>
2020-10-06 08:52:31 +0700

    Add try catch error handling to the update method for bootcamp

--- DEVCAMPER_API/controllers/bootcamps.js
+++ DEVCAMPER_API/controllers/bootcamps.js
@@ -49,20 +49,25 @@ exports.createBootcamp = async (req, res, next) => {
 // @route       PUT /api/v1/bootcamps/:id
 // @access      Private
 exports.updateBootcamp = async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
-
-    if(!bootcamp){
-        return res.status(400).json({ success: false});
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+    
+        if(!bootcamp){
+            return res.status(400).json({ success: false});
+        }
+        res.status(200).json({ success: true, data: bootcamp});   
+    } catch (error) {
+        res.status(400).json({ success: false });
     }
-    res.status(200).json({ success: true, data: bootcamp});
 }
 
 // @desc        Delete bootcamp
 // @route       DELETE /api/v1/bootcamps/:id
 // @access      Private
 exports.deleteBootcamp = async (req, res, next) => {
+
     res.status(200).json({ success: true, msg: `Delete bootcamp: ${ req.params.id}` });
 }
