8ae6a59e4aa246d1eb52ccc4db579136b83f4e9e
melonsoda20 <benedictusteddy200596@gmail.com>
2020-10-06 08:57:23 +0700

    Add a feature to delete a bootcamp record by ID

--- DEVCAMPER_API/controllers/bootcamps.js
+++ DEVCAMPER_API/controllers/bootcamps.js
@@ -68,6 +68,15 @@ exports.updateBootcamp = async (req, res, next) => {
 // @route       DELETE /api/v1/bootcamps/:id
 // @access      Private
 exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    
+        if(!bootcamp){
+            return res.status(400).json({ success: false});
+        }
 
-    res.status(200).json({ success: true, msg: `Delete bootcamp: ${ req.params.id}` });
-}
+        res.status(200).json({ success: true, data: {}});   
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
+};
