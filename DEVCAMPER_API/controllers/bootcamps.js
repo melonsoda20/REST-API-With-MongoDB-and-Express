81d6d9f5c66aec354da7f1eac5df007b7ebfc64b
melonsoda20 <benedictusteddy200596@gmail.com>
2020-10-06 08:28:43 +0700

    Add a method to create bootcamps and store it into Mongo database

--- DEVCAMPER_API/controllers/bootcamps.js
+++ DEVCAMPER_API/controllers/bootcamps.js
@@ -1,9 +1,16 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps', hello: req.hello });
-} 
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({success: true, data: bootcamps});
+    } catch (error) {
+        res.status(400).json({ success: false });   
+    }
+}; 
 
 // @desc        Get single bootcamp
 // @route       GET /api/v1/bootcamps/:id
@@ -15,8 +22,18 @@ exports.getBootcamp = (req, res, next) => {
 // @desc        Create new bootcamp
 // @route       POST /api/v1/bootcamps
 // @access      Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+exports.createBootcamp = async (req, res, next) => {
+    try{
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+    }
+    catch(err){
+        res.status(400).json({ success: false });
+    }
 } 
 
 // @desc        Update bootcamp
