2f9c0f471e90be4ccf86be06c1449d37ae147ba5
melonsoda20 <benedictusteddy200596@gmail.com>
2020-10-06 08:33:07 +0700

    Return a not found exception when the bootcamp record does not exists in the getbootcamp method

--- DEVCAMPER_API/controllers/bootcamps.js
+++ DEVCAMPER_API/controllers/bootcamps.js
@@ -18,6 +18,10 @@ exports.getBootcamps = async (req, res, next) => {
 exports.getBootcamp = async(req, res, next) => {
     try {
         const bootcamp  = await Bootcamp.findById(req.params.id);
+        
+        if(!bootcamp){
+            return res.status(400).json({ success: false});
+        }
         res.status(200).json({ success: true, data: bootcamp});
     } catch (error) {
         res.status(400).json({ success: false });  
