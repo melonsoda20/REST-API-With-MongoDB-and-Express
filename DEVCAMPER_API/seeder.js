668962e1616885020fa0ba0254f4ba25080d302b
melonsoda20 <benedictusteddy200596@gmail.com>
2020-10-06 21:27:02 +0700

    Create model for courses and its seeder

--- DEVCAMPER_API/seeder.js
+++ DEVCAMPER_API/seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,10 +22,13 @@ mongoose.connect(process.env.MONGO_URI, {
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
 
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
+
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (error) {
@@ -37,6 +41,7 @@ const deleteData = async () => {
     try {
         // Withouth any params, Bootcamp.deleteMany will delete all record that uses the Bootcamp model
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (error) {
