ff0fec656172bb6af7ee60b4e2811c7c9d7b1c53
melonsoda20 <benedictusteddy200596@gmail.com>
2020-10-07 10:33:29 +0700

    Move filtering methods into its own middleware, and implement it in getCourses method

--- DEVCAMPER_API/routes/bootcamps.js
+++ DEVCAMPER_API/routes/bootcamps.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { getBootcamp, getBootcamps, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius, bootcampPhotoUpload } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
@@ -19,7 +22,7 @@ router
 
 router
     .route('/')
-    .get(getBootcamps)
+    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
     .post(createBootcamp);
 
 router
