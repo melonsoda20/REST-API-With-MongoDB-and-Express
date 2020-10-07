df33d0f811aa54755afaa2e4ddba6624366aef91
melonsoda20 <benedictusteddy200596@gmail.com>
2020-10-07 16:28:33 +0700

    Add a authorization by role feature whenever the user is about to do a POST, PUT, and DELETE method

--- DEVCAMPER_API/routes/courses.js
+++ DEVCAMPER_API/routes/courses.js
@@ -6,7 +6,7 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true});
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 router
     .route('/')
@@ -17,8 +17,8 @@ router
         }), 
         getCourses
     )
-    .post(protect, addCourse);
+    .post(protect, authorize('publisher', 'admin'), addCourse);
 
-router.route('/:id').get(getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
+router.route('/:id').get(getCourse).put(protect, authorize('publisher', 'admin'), updateCourse).delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
