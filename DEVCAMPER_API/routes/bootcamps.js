b7ef6b67b196db096960c5d3257459af93e6e8fe
melonsoda20 <benedictusteddy200596@gmail.com>
2020-10-07 10:11:33 +0700

    Add feature where user can upload bootcamp image

--- DEVCAMPER_API/routes/bootcamps.js
+++ DEVCAMPER_API/routes/bootcamps.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getBootcamp, getBootcamps, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius } = require('../controllers/bootcamps');
+const { getBootcamp, getBootcamps, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius, bootcampPhotoUpload } = require('../controllers/bootcamps');
 
 // Include other resource routers
 const courseRouter = require('./courses');
@@ -12,6 +12,10 @@ router.use('/:bootcampId/courses', courseRouter);
 router 
     .route('/radius/:zipcode/:distance')
     .get(getBootcampsInRadius);
+
+router
+    .route('/:id/photo')
+    .put(bootcampPhotoUpload);
 
 router
     .route('/')
